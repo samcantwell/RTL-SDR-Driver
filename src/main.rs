0ff1fe4ade0855e9e3a435cf1650a3dac4f56117
Sam Cantwell <sam@samcantwell.au>
2026-04-25 10:52:04 +1000

    Cleaned error handling

--- src/main.rs
+++ src/main.rs
@@ -4,40 +4,19 @@ mod tuner;
 
 fn main() {
     let interface = match rtlsdr::interface() {
-        Ok(interface) => {
-            if let Some(interface) = interface {
-                interface
-            } else {
-                eprintln!("Could not find RTL-SDR");
-                return;
-            }
-        }
-        Err(e) => {
-            eprintln!("Failed to return interface: {e}");
-            return;
-        }
+        Ok(Some(interface)) => interface,
+        Ok(None) => { eprintln!("Could not find RTL-SDR"); return; }
+        Err(e) => { eprintln!("USB error: {e}"); return; }
     };
 
     let interface = match rtlsdr::init(interface) {
         Ok(interface) => interface,
-        Err(e) => {
-            eprintln!("Failed to initialise rtlsdr: {e}");
-            return;
-        }
+        Err(e) => { eprintln!("Initilisation failed: {e}"); return; }
     };
 
     let _tuner = match tuner::detect_tuner(&interface) {
-        Ok(tuner) => {
-            if let Some(tuner) = tuner {
-                tuner
-            } else {
-                eprintln!("Tuner not found");
-                return;
-            }
-        }
-        Err(e) => {
-            eprintln!("Tuner detection failed: {e}");
-            return;
-        }
+        Ok(Some(tuner)) => tuner,
+        Ok(None) => { eprintln!("Tuner not found"); return; }
+        Err(e) => { eprintln!("Tuner detection failed: {e}"); return; }
     };
 }
