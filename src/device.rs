f7bb4e27c1ee9b4317f5d4efa517adc3c89dea58
Sam Cantwell <sam@samcantwell.au>
2026-07-11 19:06:27 +1000

    Cleaned minimal implementation

--- src/device.rs
+++ src/device.rs
@@ -8,6 +8,7 @@ use std::time::Duration;
 const RTL_VID: u16 = 0x0bda;
 const RTL_PID: u16 = 0x2838;
 
+/*
 const FIR_COEFFICIENTS: [i16; 16] = [
     -54, -36, -41, -40, -32, -14, 14, 53, 101, 156, 215, 273, 327, 372, 404, 421,
 ];
@@ -16,6 +17,11 @@ const TUNER_INIT_ARRAY: [u8; 27] = [
     0x80, 0x13, 0x70, 0xc0, 0x40, 0xdb, 0x6b, 0xeb, 0x53, 0x75, 0x68, 0x6c, 0xbb, 0x80, 0x31, 0x0f,
     0x00, 0xc0, 0x30, 0x48, 0xec, 0x60, 0x00, 0x24, 0xdd, 0x0e, 0x40,
 ];
+*/
+
+const R820T_DEV_ADDR: u16 = 0x0034;
+
+const TIMEOUT_DURATION: Duration = Duration::from_millis(300);
 
 enum Block {
     Usb = 1,
@@ -111,102 +117,58 @@ impl Device {
         // Enable rawIQ mode and disable DAGC
         // TODO: why disable DAGC?
         self.write_demod_reg(0, 0x19, &[0x05])?;
+        // Init FSM register
         self.write_demod_reg(1, 0x94, &[0x0f])?;
+        // ADC_I/ADC_Q datapath
         self.write_demod_reg(0, 0x06, &[0x80])?;
+        // Open I2C repeater
         self.write_demod_reg(1, 0x01, &[0x18])?;
+        // Set AGC target level
         self.write_demod_reg(1, 0x03, &[0x80])?;
+        // Set AGC loop gain
         self.write_demod_reg(1, 0x04, &[0xcc])?;
+        // Only enable in-phase ADC input
         self.write_demod_reg(0, 0x08, &[0x4d])?;
 
+        // Tuner initial writes
         self.write_reg(
             Block::I2c,
-            0x0034,
+            R820T_DEV_ADDR,
             &[0x05, 0x80, 0x13, 0x70, 0xc0, 0x40, 0xdb, 0x6b],
         )?;
         self.write_reg(
             Block::I2c,
-            0x0034,
+            R820T_DEV_ADDR,
             &[0x13, 0x31, 0x0f, 0x00, 0xc0, 0x30, 0x48, 0xec],
         )?;
 
+        // Fix spectral inversion
         self.write_demod_reg(1, 0x15, &[0x01])?;
+        // Resampler ratio
         self.write_demod_reg(1, 0x9f, &[0x03, 0x84])?;
+        // Demod soft reset
         self.write_demod_reg(1, 0x01, &[0x14])?;
+        // Release soft reset and turn i2c back on
         self.write_demod_reg(1, 0x01, &[0x18])?;
 
-        self.write_reg(Block::I2c, 0x34, &[0x1e, 0x4e])?;
+        // Enable channel filter extension
+        self.write_reg(Block::I2c, R820T_DEV_ADDR, &[0x1e, 0x4e])?;
+        // Set DDC IF frequency
         self.write_demod_reg(1, 0x19, &[0x3c])?;
         self.write_demod_reg(1, 0x1a, &[0x99])?;
-        self.write_reg(Block::I2c, 0x34, &[0x10, 0x84])?;
-        self.write_reg(Block::I2c, 0x34, &[0x14, 0x0b])?;
-        self.write_reg(Block::I2c, 0x34, &[0x16, 0x76])?;
-        self.write_reg(Block::I2c, 0x34, &[0x0c, 0xf0])?;
+        // PLL mixer divider number
+        self.write_reg(Block::I2c, R820T_DEV_ADDR, &[0x10, 0x84])?;
+        // PLL integer word
+        self.write_reg(Block::I2c, R820T_DEV_ADDR, &[0x14, 0x0b])?;
+        // PLL sigma-delta
+        self.write_reg(Block::I2c, R820T_DEV_ADDR, &[0x16, 0x76])?;
+        // VGA auto and ADC input path
+        self.write_reg(Block::I2c, R820T_DEV_ADDR, &[0x0c, 0xf0])?;
 
         Ok(())
     }
+
     /*
-    fn init(&self) -> Result<(), Error> {
-        // Initialise USB endpoint
-        self.write_reg(Block::Usb, 0x2000, &[0x09])?;
-        self.write_reg(Block::Usb, 0x2158, &[0x02, 0x00])?;
-        self.write_reg(Block::Usb, 0x2148, &[0x02, 0x10])?;
-
-        // Power on demodulator
-        // TODO: Check 0x22 write is needed
-        self.write_reg(Block::Usb, 0x300b, &[0x22])?;
-        self.write_reg(Block::Usb, 0x3000, &[0xe8])?;
-
-        // Reset demodulator
-        self.write_demod_reg(1, 0x01, &[0x14])?;
-        self.write_demod_reg(1, 0x01, &[0x10])?;
-
-        // TODO: Check this is needed
-        // Disable spectrum inversion and adjacent channel rejection
-        self.write_demod_reg(1, 0x15, &[0x00])?;
-
-        // TODO: Check what each register in this write does, not all documented
-        // Clear DDC stale state and clear IF, it is set later
-        self.write_demod_reg(1, 0x16, &[0x00, 0x00, 0x00, 0x00, 0x00, 0x00])?;
-
-        // Set FIR coefficients
-        self.write_demod_reg(1, 0x1c, &Self::convert_fir(FIR_COEFFICIENTS))?;
-
-        /*
-        // Below demod writes copied from driver code
-        /* enable SDR mode, disable DAGC (bit 5) */
-        self.write_demod_reg(0, 0x19, &[0x05])?;
-
-        /* init FSM state-holding register */
-        self.write_demod_reg(1, 0x93, &[0xf0])?;
-        self.write_demod_reg(1, 0x94, &[0x0f])?;
-
-        /* disable AGC (en_dagc, bit 0) (this seems to have no effect) */
-        self.write_demod_reg(1, 0x11, &[0x00])?;
-
-        /* disable RF and IF AGC loop */
-        self.write_demod_reg(1, 0x04, &[0x00])?;
-
-        /* disable PID filter (enable_PID = 0) */
-        self.write_demod_reg(0, 0x61, &[0x60])?;
-
-        /* opt_adc_iq = 0, default ADC_I/ADC_Q datapath */
-        self.write_demod_reg(0, 0x06, &[0x80])?;
-
-        /* Enable Zero-IF mode (en_bbin bit), DC cancellation (en_dc_est),
-         * IQ estimation/compensation (en_iq_comp, en_iq_est) */
-        self.write_demod_reg(1, 0xb1, &[0x1b])?;
-
-        /* disable 4.096 MHz clock output on pin TP_CK0 */
-        self.write_demod_reg(0, 0x0d, &[0x83])?;
-        */
-
-        // Unstall
-        self.write_reg(Block::Usb, 0x2148, &[0x00, 0x00])?;
-
-        Ok(())
-    }
-    */
-
     #[allow(clippy::cast_possible_truncation)]
     #[allow(clippy::cast_sign_loss)]
     fn convert_fir(fir: [i16; 16]) -> Vec<u8> {
@@ -225,34 +187,32 @@ impl Device {
     }
 
     fn detect_tuner(&self) -> Result<(), Error> {
-        self.i2c_open()?;
+    self.i2c_open()?;
 
-        let tuner = self.i2c_read_reg(0x34, 0x00, 1)?[0];
-        if tuner != 0x69 {
-            return Err(Error::TunerNotFound);
-        }
+    let tuner = self.i2c_read_reg(0x00, 1)?[0];
+    if tuner != 0x69 {
+    return Err(Error::TunerNotFound);
+    }
 
-        self.i2c_close()?;
-        Ok(())
+    self.i2c_close()?;
+    Ok(())
     }
 
     fn init_tuner(&self) -> Result<(), Error> {
-        self.i2c_open()?;
+    self.i2c_open()?;
 
-        self.i2c_write_reg(0x34, 0x00, &[0x05, 0b1001_0110, 0b0110_1001])?;
-        let regs = self.i2c_read_reg(0x34, 0x00, 16)?;
-        for reg in regs {
-            println!("0x{reg:02x} 0b{reg:08b}");
-        }
-        //self.i2c_write_reg(0x34, 0x05, &TUNER_INIT_ARRAY)?;
-        //dbg!(self.i2c_read_reg(0x34, 0x00, 32)?);
-
-        self.i2c_close()?;
-        Ok(())
+    self.i2c_write_reg(0x00, &[0x05, 0b1001_0110, 0b0110_1001])?;
+    let regs = self.i2c_read_reg(0x00, 16)?;
+    for reg in regs {
+    println!("0x{reg:02x} 0b{reg:08b}");
     }
-}
+    //self.i2c_write_reg(0x34, 0x05, &TUNER_INIT_ARRAY)?;
+    //dbg!(self.i2c_read_reg(0x34, 0x00, 32)?);
 
-impl Device {
+    self.i2c_close()?;
+    Ok(())
+    }
+    */
     #[expect(dead_code)]
     fn read_reg(&self, block: Block, addr: u16, length: u16) -> Result<Vec<u8>, TransferError> {
         self.interface
@@ -265,7 +225,7 @@ impl Device {
                     index: (block as u16) << 8,
                     length,
                 },
-                Duration::from_millis(300),
+                TIMEOUT_DURATION,
             )
             .wait()
     }
@@ -292,7 +252,7 @@ impl Device {
                     //data: &reversed,
                     data,
                 },
-                Duration::from_millis(300),
+                TIMEOUT_DURATION,
             )
             .wait()
     }
@@ -309,7 +269,7 @@ impl Device {
                     index: u16::from(page),
                     length,
                 },
-                Duration::from_millis(300),
+                TIMEOUT_DURATION,
             )
             .wait()
     }
@@ -325,55 +285,62 @@ impl Device {
                     index: u16::from(page) | 0x10,
                     data,
                 },
-                Duration::from_millis(300),
+                TIMEOUT_DURATION,
             )
             .wait()
     }
-
-    fn i2c_open(&self) -> Result<(), Error> {
-        self.write_demod_reg(1, 0x01, &[0x18])?;
-        Ok(())
-    }
-
-    fn i2c_close(&self) -> Result<(), Error> {
-        self.write_demod_reg(1, 0x01, &[0x10])?;
-        Ok(())
-    }
-
-    fn i2c_read_reg(
-        &self,
-        dev_addr: u8,
-        reg_addr: u8,
-        length: u16,
-    ) -> Result<Vec<u8>, TransferError> {
-        self.interface
-            .control_in(
-                ControlIn {
-                    control_type: ControlType::Vendor,
-                    recipient: Recipient::Device,
-                    request: 0,
-                    value: u16::from(reg_addr) << 8 | u16::from(dev_addr),
-                    index: (Block::I2c as u16) << 8,
-                    length,
-                },
-                Duration::from_millis(300),
-            )
-            .wait()
-    }
-
-    fn i2c_write_reg(&self, dev_addr: u8, reg_addr: u8, data: &[u8]) -> Result<(), TransferError> {
-        self.interface
-            .control_out(
-                ControlOut {
-                    control_type: ControlType::Vendor,
-                    recipient: Recipient::Device,
-                    request: 0,
-                    value: u16::from(reg_addr) << 8 | u16::from(dev_addr),
-                    index: (Block::I2c as u16) << 8 | 0x10,
-                    data,
-                },
-                Duration::from_millis(300),
-            )
-            .wait()
-    }
+} /*
+fn i2c_open(&self) -> Result<(), Error> {
+self.write_demod_reg(1, 0x01, &[0x18])?;
+Ok(())
 }
+
+fn i2c_close(&self) -> Result<(), Error> {
+self.write_demod_reg(1, 0x01, &[0x10])?;
+Ok(())
+}
+
+fn i2c_read_reg(&self, reg_addr: u8, length: u16) -> Result<Vec<u8>, TransferError> {
+self.interface
+.control_in(
+encode_read_i2c(self.tuner.i2c_addr, reg_addr, length),
+TIMEOUT_DURATION,
+)
+.wait()
+}
+
+fn i2c_write_reg(&self, reg_addr: u8, data: &[u8]) -> Result<(), TransferError> {
+// TODO: Add max write limits
+// TODO: Add shadow cache
+self.interface
+.control_out(
+encode_write_i2c(self.tuner.i2c_addr, reg_addr, data),
+TIMEOUT_DURATION,
+)
+.wait()
+}
+}
+
+fn encode_read_i2c(dev_addr: u16, reg_addr: u8, length: u16) -> ControlIn {
+ControlIn {
+control_type: ControlType::Vendor,
+recipient: Recipient::Device,
+request: 0,
+value: u16::from(reg_addr) << 8 | dev_addr,
+index: (Block::I2c as u16) << 8,
+length,
+}
+}
+
+fn encode_write_i2c(dev_addr: u16, reg_addr: u8, data: &[u8]) -> ControlOut {
+ControlOut {
+control_type: ControlType::Vendor,
+recipient: Recipient::Device,
+request: 0,
+value: dev_addr,
+index: (Block::I2c as u16) << 8 | 0x10,
+// TODO: Add register addressing in data
+data,
+}
+}
+*/
